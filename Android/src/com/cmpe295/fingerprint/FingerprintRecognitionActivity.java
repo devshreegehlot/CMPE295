fd7a9f7c186a1849fa92b643dd2493935d44af90
Devshree Gehlot <devshree@gmail.com>
2012-03-20 21:39:34 -0700

    android

---
+++ Android/src/com/cmpe295/fingerprint/FingerprintRecognitionActivity.java
@@ -0,0 +1,13 @@
+package com.cmpe295.fingerprint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FingerprintRecognitionActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
